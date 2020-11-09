b8821a2aa94f91ca631bcf02287e722a55b1e73f
Henrique Tinoco <liquelt@gmail.com>
2020-11-09 15:26:54 +0000

    Tentativa SaverRecord with Comments

--- Models/Valoresc.cs
+++ Models/Valoresc.cs
@@ -9,32 +9,21 @@ using System.Xml.Schema;
 
 namespace YClient_htinoco2.Models
 {
-        //   public class Valoresc : DbContext
+    public class Valorestab : PageModel
+    {
+        //public Valorestab()
         //{
 
-        //    public Valoresc(DbContextOptions<Valoresc> options) : base(options) { }
-        //    protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //    {
-        //        base.OnModelCreating(modelBuilder);
-        //        modelBuilder.Entity<Valorestab>().HasNoKey();
-        //    }
         //}
+        public int NumeId { get; set; }
+        public decimal Ndecimal { get; set; }
 
-        public class Valorestab
-        {
-            //public Numero()
-            //{
-
-            //}
-            public int NumeId { get; set; }
-            public decimal Ndecimal { get; set; }
-
-            //    public Numero(int numeId, decimal ndecimal)
-            //    {
-            //        this.NumeId = numeId;
-            //        this.Ndecimal = ndecimal;
-            //    }
-            //}
-        }
+        //public Valorestab(int numeId, decimal ndecimal)
+        //{
+        //    this.NumeId = numeId;
+        //    this.Ndecimal = ndecimal;
+        //}
     }
+}
+    
 
